b9f3cbef6e0afb25659211749c003dbf62dab302
Greg Doud <gpdoud@gmail.com>
2020-06-18 15:35:35 -0400

    Add project files.

---
+++ StudentClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StudentClass {
+    class Program {
+        static void Main(string[] args) {
+
+            var jada = new Student();
+            jada.Name = "Jada";
+            jada.Id = 1;
+            jada.GPA = 3.5;
+            jada.SAT = 1600;
+            jada.TimesTardy = 112;
+            Console.WriteLine($"Jada's name is {jada.Name}");
+
+            var steve = new Student();
+            steve.Name = "Steve";
+            steve.Id = 2;
+            steve.GPA = 4.0;
+            steve.SAT = 1599;
+            steve.TimesTardy = 0;
+            var steveString = steve.GetValues();
+            Console.WriteLine($"Steve: {steveString}");
+            // Calls the BC method for steve's instance
+            Console.WriteLine($"Steve's BC score is {steve.BC(2)}");
+
+            var dave = new Student("Dave", 100, 1250, 3.5, 1);
+        }
+    }
+}
---
+++ StudentClass/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClass {
+
+    class Student {
+        public double GPA;
+        public int SAT;
+        public int TimesTardy;
+        public string Name;
+        public int Id;
+
+        private double SecretNumberCalculation(double Bnumber, int SecretNumber) {
+            return Bnumber / SecretNumber;
+        }
+
+        public double BC(int SecretNumber) {
+            var dblSAT = SAT * 2;
+            var b = dblSAT * GPA;
+            var bc = SecretNumberCalculation(b, SecretNumber);
+            return bc;
+        }
+
+        public string GetValues() {
+            return $"{Id}|{Name}|{SAT}|{GPA}|{TimesTardy}";
+        }
+
+        public Student() { // default constructor
+            this.Id = 0;
+            this.GPA = 2.0;
+            this.SAT = 1000;
+        }
+        public Student(string Name, int Id, int SAT, double GPA, int Tardy) {
+            this.Name = Name;
+            this.Id = Id;
+            this.SAT = SAT;
+            this.GPA = GPA;
+            this.TimesTardy = Tardy;
+        }
+    }
+}
